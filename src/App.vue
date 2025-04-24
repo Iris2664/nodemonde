6380a51a5a33a6ab6cc7a6a45c4ffab05ba8a464
HINATA Ayame <137654183+Iris2664@users.noreply.github.com>
2025-04-25 02:45:49 +0900

    done

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   import { ref } from 'vue';
   import Card from "./components/Card.vue";
 
+  const input = ref("");
   const countries = ref();
+
   async function fetchCountries() {
-    const response = await fetch("https://restcountries.com/v3.1/name/japan");
+    const response = await fetch(`https://restcountries.com/v3.1/name/${input.value}`);
     const date = await response.json();
     countries.value = date;
   }
@@ -12,7 +14,12 @@
 
 <template>
   <h1>Country Info</h1>
-  <button @click="fetchCountries">Fetch</button>
+
+  <form @submit.prevent="fetchCountries">
+    <input v-model="input" placeholder="coutrty name" />
+    <button @click="fetchCountries">Fetch</button>  
+  </form>
+  
   
   <Card
     v-for="country in countries"
