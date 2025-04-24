325f8b90a7621d0d6ff0e8a86e4b33670ba126f1
HINATA Ayame <137654183+Iris2664@users.noreply.github.com>
2025-04-24 16:41:25 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue';
+
+  const countries = ref();
+  async function fetchCountries() {
+    const response = await fetch("https://restcountries.com/v3.1/name/japan");
+    const date = await response.json();
+    countries.value = date;
+  }
 </script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Country Info</h1>
+  <button @click="fetchCountries">Fetch</button>
+  <article v-for="country in countries">
+    <div>{{ country.name.common }}</div>
+    <div>{{ country.flags.png }}</div>
+    <div>{{ country.tld }}</div>
+  </article>
 </template>
 
 <style scoped>
